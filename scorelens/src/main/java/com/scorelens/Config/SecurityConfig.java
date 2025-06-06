6270f3d1c031258e7cfa3aac142548c6ba3b6fb5
mngdinh <minhdc.uni@gmail.com>
2025-06-06 14:19:23 +0700

    websocket for real time message, index.html

--- scorelens/src/main/java/com/scorelens/Config/SecurityConfig.java
+++ scorelens/src/main/java/com/scorelens/Config/SecurityConfig.java
@@ -35,8 +35,7 @@ public class SecurityConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-
-        corsConfiguration.addAllowedOrigin("http://localhost:5173");
+        corsConfiguration.addAllowedOriginPattern("*"); // mở rộng cho tất cả các port localhost
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
 
