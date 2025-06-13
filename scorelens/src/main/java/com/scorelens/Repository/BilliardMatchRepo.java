f2fa447ef61b69923a4254d8efde8b9000d1fa37
ToTuyen04 <tuyentmse183888@fpt.edu.vn>
2025-06-13 11:26:49 +0700

    fix docker file

--- scorelens/src/main/java/com/scorelens/Repository/BilliardMatchRepo.java
+++
@@ -1,7 +0,0 @@
-package com.scorelens.Repository;
-
-import com.scorelens.Entity.BilliardMatch;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BilliardMatchRepo extends JpaRepository<BilliardMatch, Integer> {
-}
