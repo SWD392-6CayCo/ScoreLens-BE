8bbd2dfe238ac80aaf98f09d51513a5fe52f8546
ToTuyen04 <tuyentmse183888@fpt.edu.vn>
2025-06-15 22:47:56 +0700

    change to nullable field: winner, endTime, byStaff, byCustomer

--- scorelens/src/main/java/com/scorelens/Entity/BilliardMatch.java
+++ scorelens/src/main/java/com/scorelens/Entity/BilliardMatch.java
@@ -2,6 +2,8 @@ package com.scorelens.Entity;
 
 import com.scorelens.Enums.MatchStatus;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Null;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -18,10 +20,12 @@ public class BilliardMatch {
     @Column(name = "billiardMatchID")
     private int billiardMatchID;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "billardTableID", nullable = false)
     private BilliardTable billardTableID;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "modeID", nullable = false)
     private Mode modeID;
@@ -37,19 +41,23 @@ public class BilliardMatch {
     @Column(name = "winner", length = 50)
     private String winner;
 
+    @NotNull
     @Column(name = "totalRound")
     private int totalRound;
 
+    @NotNull
     @Column(name = "startTime")
     private LocalDateTime startTime;
 
     @Column(name = "endTime")
     private LocalDateTime endTime;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     @Column(name = "status")
     private MatchStatus status; //-- pending, ongoing, completed, cancelled, forfeited
 
+    @NotNull
     @Column(name = "code")
     private String code;
 
