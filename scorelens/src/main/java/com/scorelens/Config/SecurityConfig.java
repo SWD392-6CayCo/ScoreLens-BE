273ccbf0e5d743e6ce15ea2fef8cc8e6c51546ad
mngdinh <minhdc.uni@gmail.com>
2025-07-17 23:51:56 +0700

    Update SecurityConfig.java

--- scorelens/src/main/java/com/scorelens/Config/SecurityConfig.java
+++ scorelens/src/main/java/com/scorelens/Config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
             "/v*/ping",
             "/index.html",
             "/ws/**",
-            "/ws-native", "/ws-native/**", "**/ws-native",
+            "/ws-native", "/ws-native/**"
 
 
 
